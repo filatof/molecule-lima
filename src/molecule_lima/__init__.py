1b414826f37072a7170ed238f409e8a3d01b14fc
Ivan A. Filatoff <fill@MacBookAir.local>
2025-11-10 16:36:44 +0300

    bugfix

--- src/molecule_lima/__init__.py
+++ src/molecule_lima/__init__.py
@@ -1,6 +1,8 @@
 """Molecule Lima Driver."""
+
+__version__ = "0.0.1"
+__author__ = "Ivan Filatof"
+
 from molecule_lima.driver import Lima
 
-__version__ = '0.0.1'
-__author__ = "Ivan Filatof"
 __all__ = ['Lima']
